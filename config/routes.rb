7a3b87ffdc94e5bfa85f82dd75a193b762d642c0
nyacos <m_kutipatti23@yahoo.co.jp>
2023-02-22 23:50:05 +0900

    erb commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'boards/index'
+  get 'boards/new'
+  get 'boards/show'
+  get 'boards/edit'
   resources :posts
   root :to => 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
